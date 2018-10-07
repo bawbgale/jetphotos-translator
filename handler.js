91567a02d5ad395c5685108a2247d67effd6a225
Ian Cole <ian.cole@me.com>
2018-10-07 11:25:35 -0700

    Update getjetphoto function to take in param

--- handler.js
+++ handler.js
@@ -3,18 +3,19 @@ const request = require('axios');
 const cheerio = require('cheerio');
 
 module.exports.getjetphoto = async (event, context, callback) => {
-  return request('https://www.jetphotos.com/photo/keyword/n3476g')
+  const tailNum = event.tailNum;
+  return request(`https://www.jetphotos.com/photo/keyword/${tailNum}`)
     .then(({data}) => {
-      // const $ = cheerio.load(data);
-      // const photoElements = $('img.result__photo');
-      // const photos = [];
-      // photoElements.each((i, el) => {
-      //   photos.push($(el).attr('src'));
-      // });
-      var html = '<html><img style="max-width: 500px;" src="//cdn.jetphotos.com/full/5/83475_1495955378.jpg"/></html>';
+      const $ = cheerio.load(data);
+      const photoElements = $('img.result__photo');
+      const photos = [];
+      photoElements.each((i, el) => {
+        photos.push($(el).attr('src'));
+      });
+      var html = `<html><img style="max-width: 500px;" src="${photos[0]}"/></html>`;
       context.succeed(html);
     })
     .catch((err) => {
-      callback(null, {err});
+      context.succeed(err.response);
     });
 };
