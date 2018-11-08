df38391a0c41f4370b4fafbbeb4dadee34310cbc
Bob Gale <bawbgale@gmail.com>
2018-11-08 11:08:56 -0800

    lint fixes

--- handler.js
+++ handler.js
@@ -1,21 +1,21 @@
-'use strict';
-const request = require('axios');
-const cheerio = require('cheerio');
+'use strict'
+const request = require('axios')
+const cheerio = require('cheerio')
 
 module.exports.getjetphoto = async (event, context, callback) => {
-  const tailNum = event.tailNum;
+  const tailNum = event.tailNum
   return request(`https://www.jetphotos.com/photo/keyword/${tailNum}`)
-    .then(({data}) => {
-      const $ = cheerio.load(data);
-      const photoElements = $('img.result__photo');
-      const photos = [];
+    .then(({ data }) => {
+      const $ = cheerio.load(data)
+      const photoElements = $('img.result__photo')
+      const photos = []
       photoElements.each((i, el) => {
-        photos.push($(el).attr('src'));
-      });
-      var html = `<html><img style="max-width: 500px;" src="${photos[0]}"/></html>`;
-      context.succeed(html);
+        photos.push($(el).attr('src'))
+      })
+      var html = `<html><img style="max-width: 500px;" src="${photos[0]}"/></html>`
+      context.succeed(html)
     })
     .catch((err) => {
-      context.succeed(err.response);
-    });
-};
+      context.succeed(err.response)
+    })
+}
