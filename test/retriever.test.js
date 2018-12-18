ab944631ca5eab85d7cfb592889fff31599f0062
Bob Gale <bawbgale@gmail.com>
2018-12-17 20:54:41 -0800

    add caching tests

--- test/retriever.test.js
+++ test/retriever.test.js
@@ -1,6 +1,8 @@
 const chai = require('chai')
 const expect = chai.expect
 const moxios = require('moxios')
+const fs = require('fs')
+const mockfs = require('mock-fs')
 
 const retriever = require('../retriever.js')
 
@@ -11,7 +13,38 @@ const mochaAsync = (fn) => {
     })
   }
 }
-
+let mockData = `
+<html>
+  <body>
+    <div class="result__section result__section--photo-wrapper">
+      <a href="/photo/photo_id1" class="result__photoLink">
+        <img src="photo_url1" class="result__photo"/>
+      </a>
+    </div>
+    <div class="result__section result__section--photo-wrapper">
+      <a href="/photo/photo_id2" class="result__photoLink">
+        <img src="photo_url2" class="result__photo"/>
+      </a>
+    </div>
+  </body>
+</html>
+`
+let cachedPage = `
+<html>
+  <body>
+    <div class="result__section result__section--photo-wrapper">
+      <a href="/photo/photo_id1" class="result__photoLink">
+        <img src="cached_photo_url1" class="result__photo"/>
+      </a>
+    </div>
+    <div class="result__section result__section--photo-wrapper">
+      <a href="/photo/photo_id2" class="result__photoLink">
+        <img src="cached_photo_url2" class="result__photo"/>
+      </a>
+    </div>
+  </body>
+</html>
+`
 describe('getjetphotos', () => {
   beforeEach(() => {
     moxios.install()
@@ -22,22 +55,6 @@ describe('getjetphotos', () => {
   })
 
   it('extracts photo URLs from jetphotos', mochaAsync(async () => {
-    let mockData = `
-        <html>
-          <body>
-            <div class="result__section result__section--photo-wrapper">
-              <a href="/photo/photo_id1" class="result__photoLink">
-                <img src="photo_url1" class="result__photo"/>
-              </a>
-            </div>
-            <div class="result__section result__section--photo-wrapper">
-              <a href="/photo/photo_id2" class="result__photoLink">
-                <img src="photo_url2" class="result__photo"/>
-              </a>
-            </div>
-          </body>
-        </html>
-      `
     moxios.wait(() => {
       const request = moxios.requests.mostRecent()
       request.respondWith({ status: 200, response: mockData })
@@ -47,5 +64,53 @@ describe('getjetphotos', () => {
     let expectedPhotos = ['photo_url1', 'photo_url2']
     const result = await retriever.getjetphotos(tailNum)
     expect(result).to.eql(expectedPhotos)
+  }))
+
+  it('saves pages to cache', mochaAsync(async () => {
+    mockfs({
+      'cache': {
+        'cachedTailNum.html': cachedPage
+      }
+    })
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({ status: 200, response: mockData })
+    })
+
+    let tailNum = 'uncachedTailNum'
+    let expectedPhotos = ['photo_url1', 'photo_url2']
+    let result = await retriever.getjetphotos(tailNum, true)
+    expect(result).to.eql(expectedPhotos)
+    fs.readFile(`cache/${tailNum}.html`, 'utf8', (err, fileData) => {
+      if (err) throw err
+      expect(fileData).to.eql(mockData)
+    })
+  }))
+
+  it('extracts from cache if page found there', mochaAsync(async () => {
+
+    mockfs({
+      'cache': {
+        'cachedTailNum.html': cachedPage
+      }
+    })
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({ status: 200, response: mockData })
+    })
+
+    let tailNum = 'cachedTailNum'
+    let expectedPhotos = ['cached_photo_url1', 'cached_photo_url2']
+    let result = await retriever.getjetphotos(tailNum, true)
+    expect(result).to.eql(expectedPhotos)
+
+    tailNum = 'uncachedTailNum'
+    expectedPhotos = ['photo_url1', 'photo_url2']
+    result = await retriever.getjetphotos(tailNum)
+    expect(result).to.eql(expectedPhotos)
+
+    mockfs.restore()
   }))
 })
