c6b42d779b5b73285ce833bc45405da727cef019
Bob Gale <bawbgale@gmail.com>
2019-01-27 15:59:09 -0800

    cross-platform refactoring

--- handler.js
+++ handler.js
@@ -2,99 +2,69 @@
 const path = require('path')
 const retriever = require(path.join(__dirname, '//retriever.js'))
 
-module.exports.getjetphoto = async (event, context, callback) => {
-  let response = { headers: { 'Content-Type': 'text/html' } }
+module.exports.getjetphoto_aws = async (event, context) => {
+  let tailNum
   if (event.tailNum || event.queryStringParameters.tailNum || JSON.parse(event.body).tailNum) {
-    const tailNum = (event.tailNum || event.queryStringParameters.tailNum || JSON.parse(event.body).tailNum)
+    tailNum = (event.tailNum || event.queryStringParameters.tailNum || JSON.parse(event.body).tailNum)
+  }
+  let response = await getjetphoto(tailNum)
+  console.log(response.log)
+  return {
+    headers: { 'Content-Type': 'text/html' },
+    statusCode: response.status,
+    body: response.body
+  }
+}
+
+module.exports.getjetphoto_azure = async (context, req) => {
+  let tailNum
+  if (req.query.tailNum || req.body.tailNum) {
+    tailNum = (req.query.tailNum || req.body.tailNum)
+  }
+  let response = await getjetphoto(tailNum)
+  context.log(response.log)
+  context.res
+    .type('text/html')
+    .status(response.status)
+    .send(response.body)
+}
+
+exports.getjetphoto_google = async (req, res) => {
+  let tailNum
+  if (req.query.tailNum || req.body.tailNum) {
+    tailNum = (req.query.tailNum || req.body.tailNum)
+  }
+  let response = await getjetphoto(tailNum)
+  console.log(response.log)
+  res
+    .type('text/html')
+    .status(response.status)
+    .send(response.body)
+}
+
+async function getjetphoto (tailNum) {
+  let response = {}
+  if (tailNum) {
     const result = await retriever.getjetphotos(tailNum)
     if (!Array.isArray(result)) {
-      console.log(`Retriever error ${result}`)
-      response.statusCode = +result.match(/\[(\d+)\]/)[1] || 400
+      response.log = `Retriever error ${result}`
+      response.status = +result.match(/\[(\d+)\]/)[1] || 400
       response.body = result
-      callback(null, response)
     } else if (result.length === 0) {
-      console.log(`No photos :-(`)
-      response.statusCode = 204
+      response.log = 'No photos :-('
+      response.status = 204
       response.body = wrapHtml('No photos :-(')
-      callback(null, response)
     } else {
-      console.log(`Retrieved ${result.length} photo URLs`)
-      response.statusCode = 200
+      response.log = `Retrieved ${result.length} photo URLs`
+      response.status = 200
       response.body = wrapHtml(imgTag(result[0]))
-      callback(null, response)
     }
   } else {
-    console.log('No tailNum')
-    response.statusCode = 400
+    response.log = 'No tailNum'
+    response.status = 400
     response.body = 'Please pass a tail number on the query string or in the request body'
-    callback(null, response)
   }
-}
-
-module.exports.getjetphotoazure = async (context, req) => {
-  if (req.query.tailNum || req.body.tailNum) {
-    const tailNum = (req.query.tailNum || req.body.tailNum)
-    const result = await retriever.getjetphotos(tailNum)
-    if (!Array.isArray(result)) {
-      context.log(`Retriever error ${result}`)
-      return context.res
-        .status(result.match(/\[(\d+)\]/)[1] || 400)
-        .type('text/html')
-        .send(result)
-    } else if (result.length === 0) {
-      context.log('No photos :-(')
-      return context.res
-        .status(204)
-        .type('text/html')
-        .send('No photos :-(')
-    } else {
-      context.log(`Retrieved ${result.length} photo URLs`)
-      return context.res
-        .status(200)
-        .type('text/html')
-        .send(wrapHtml(imgTag(result[0])))
-    }
-  } else {
-    context.log('No tailNum')
-    return context.res
-      .status(400)
-      .type('text/html')
-      .send('Please pass a tail number on the query string or in the request body')
-  }
-}
-
-exports.getjetphotogoogle = async (req, res) => {
-  console.log(req)
-  if (req.query.tailNum || req.body.tailNum) {
-    const tailNum = (req.query.tailNum || req.body.tailNum)
-    const result = await retriever.getjetphotos(tailNum)
-    console.log(tailNum)
-    if (!Array.isArray(result)) {
-      console.log(`Retriever error ${result}`)
-      return res
-        .status(result.match(/\[(\d+)\]/)[1] || 400)
-        .type('text/html')
-        .send(result)
-    } else if (result.length === 0) {
-      console.log('No photos :-(')
-      return res
-        .status(204)
-        .type('text/html')
-        .send('No photos :-(')
-    } else {
-      console.log(`Retrieved ${result.length} photo URLs`)
-      return res
-        .status(200)
-        .type('text/html')
-        .send(wrapHtml(imgTag(result[0])))
-    }
-  } else {
-    console.log('No tailNum')
-    return res
-      .status(400)
-      .type('text/html')
-      .send('Please pass a tail number on the query string or in the request body')
-  }
+  return response
 }
 
 function imgTag (photo) {
